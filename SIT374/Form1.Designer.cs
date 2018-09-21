b5a8ccabd7cb151e395a875e3d7f4e2c0e4dcf40
zuhuh <38488195+zuhuh@users.noreply.github.com>
2018-09-21 20:17:29 +1000

    Add files via upload

--- SIT374/Form1.Designer.cs
+++ SIT374/Form1.Designer.cs
@@ -28,19 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             this.label1 = new System.Windows.Forms.Label();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label4 = new System.Windows.Forms.Label();
             this.DataGrid = new System.Windows.Forms.DataGridView();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.propertyBox = new System.Windows.Forms.CheckedListBox();
+            this.fieldBox = new System.Windows.Forms.CheckedListBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.GenerateBarChart = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.GenerateLineChart = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.DataGrid)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -51,7 +66,7 @@
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Padding = new System.Windows.Forms.Padding(0, 7, 0, 0);
-            this.label1.Size = new System.Drawing.Size(973, 53);
+            this.label1.Size = new System.Drawing.Size(1182, 53);
             this.label1.TabIndex = 0;
             this.label1.Text = "Ford Data Analyser";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -66,11 +81,12 @@
             this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(973, 494);
+            this.tabControl1.Size = new System.Drawing.Size(1182, 900);
             this.tabControl1.TabIndex = 1;
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.label4);
             this.tabPage1.Controls.Add(this.DataGrid);
             this.tabPage1.Controls.Add(this.button2);
             this.tabPage1.Controls.Add(this.button1);
@@ -84,6 +100,15 @@
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Import Data";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(181, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
             // 
             // DataGrid
             // 
@@ -147,24 +172,102 @@
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.splitContainer1);
             this.tabPage2.Location = new System.Drawing.Point(4, 29);
             this.tabPage2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.tabPage2.Size = new System.Drawing.Size(965, 461);
+            this.tabPage2.Size = new System.Drawing.Size(1174, 867);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Analyse Data";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // propertyBox
+            // 
+            this.propertyBox.CheckOnClick = true;
+            this.propertyBox.FormattingEnabled = true;
+            this.propertyBox.Location = new System.Drawing.Point(134, 3);
+            this.propertyBox.Name = "propertyBox";
+            this.propertyBox.Size = new System.Drawing.Size(157, 214);
+            this.propertyBox.TabIndex = 3;
+            // 
+            // fieldBox
+            // 
+            this.fieldBox.CheckOnClick = true;
+            this.fieldBox.FormattingEnabled = true;
+            this.fieldBox.Location = new System.Drawing.Point(3, 3);
+            this.fieldBox.Name = "fieldBox";
+            this.fieldBox.Size = new System.Drawing.Size(125, 214);
+            this.fieldBox.TabIndex = 2;
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(3, 3);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(724, 671);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // GenerateBarChart
+            // 
+            this.GenerateBarChart.Location = new System.Drawing.Point(51, 239);
+            this.GenerateBarChart.Name = "GenerateBarChart";
+            this.GenerateBarChart.Size = new System.Drawing.Size(173, 30);
+            this.GenerateBarChart.TabIndex = 1;
+            this.GenerateBarChart.Text = "Generate Bar Chart";
+            this.GenerateBarChart.UseVisualStyleBackColor = true;
+            this.GenerateBarChart.Click += new System.EventHandler(this.GenerateBarChart_Click);
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Location = new System.Drawing.Point(4, 4);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.GenerateLineChart);
+            this.splitContainer1.Panel1.Controls.Add(this.fieldBox);
+            this.splitContainer1.Panel1.Controls.Add(this.GenerateBarChart);
+            this.splitContainer1.Panel1.Controls.Add(this.propertyBox);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.chart1);
+            this.splitContainer1.Size = new System.Drawing.Size(1167, 860);
+            this.splitContainer1.SplitterDistance = 264;
+            this.splitContainer1.SplitterWidth = 10;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // GenerateLineChart
+            // 
+            this.GenerateLineChart.Location = new System.Drawing.Point(51, 276);
+            this.GenerateLineChart.Name = "GenerateLineChart";
+            this.GenerateLineChart.Size = new System.Drawing.Size(173, 29);
+            this.GenerateLineChart.TabIndex = 4;
+            this.GenerateLineChart.Text = "Generate Line Chart";
+            this.GenerateLineChart.UseVisualStyleBackColor = true;
+            this.GenerateLineChart.Click += new System.EventHandler(this.GenerateLineChart_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(973, 547);
+            this.ClientSize = new System.Drawing.Size(1182, 953);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.label1);
             this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
@@ -174,6 +277,12 @@
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.DataGrid)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -190,6 +299,13 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.DataGridView DataGrid;
+        private System.Windows.Forms.Button GenerateBarChart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.CheckedListBox fieldBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckedListBox propertyBox;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button GenerateLineChart;
     }
 }
 
