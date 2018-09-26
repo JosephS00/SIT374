d23b109da860edd02f9da5ed63aa1ae7611b2829
Joseph Swaby <josephswaby@gmail.com>
2018-09-26 11:05:32 +1000

    Updated Data Comparer

--- SIT374/DataCompare.cs
+++ SIT374/DataCompare.cs
@@ -10,63 +10,62 @@ namespace DataApi
     {
         public static bool AccelarationPedal(double data)
         {
-            return (data > 0 && data < 100);
+            return (data >= 0 && data <= 100);
         }
 
         public static bool RPM(double data)
         {
-            return (data > 0 && data < 10000);
+            return (data >= 0 && data <= 10000);
         }
 
         public static bool Power(double data)
         {
-            return (data > 0 && data < 1000);
+            return (data >= 0 && data <= 1000);
         }
 
         public static bool Torque(double data)
         {
-            return (data > 1 && data < 2000);
+            return (data >= 0 && data <= 2000);
         }
-
 
         public static bool Cylinders(double data)
         {
-            return (data > 1 && data < 20);
+            return (data >= 1 && data <= 20);
         }
 
         public static bool ValvesperCylinder(double data)
         {
-            return (data > 2 && data < 10);
+            return (data >= 2 && data <= 12);
         }
 
         public static bool CylinderCapacity(double data)
         {
-            return (data > 1 && data < 10000);
+            return (data >= 0 && data <= 10000);
         }
 
         public static bool TopSpeed(double data)
         {
-            return (data > 1 && data < 600);
+            return (data >= 1 && data <= 600);
         }
 
 
         public static bool Acceleration(double data)
         {
-            return (data > 1 && data < 60);
+            return (data >= 0 && data <= 60);
         }
 
         public static bool FuelConsumption(double data)
         {
-            return (data > 1 && data < 80);
+            return (data >= 0 && data <= 80);
         }
         public static bool CO2Emissions(double data)
         {
-            return (data > 0 && data < 800);
+            return (data >= 0 && data <= 800);
         }
 
         public static bool Weight(double data)
         {
-            return (data > 500 && data < 2000);
+            return (data >= 500 && data <= 2000);
         }
     }
 }
